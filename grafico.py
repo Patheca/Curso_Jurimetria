3a44bdb9167fbbcc60461b307ffecdc414cebc6a
Patheca <pathe.castilho@gmail.com>
2026-05-20 10:57:57 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -19,3 +19,19 @@ df = pd.read_excel(url, sheet_name="SENTENÇAS")
 df.head()
 st.header('ESTUDO JURIMÉTRICO SOBRE CORRELAÇÃO ARGUMENTATIVA NO MACROLITÍGIO FISCAL')
 
+df.columns = (
+    df.columns.str.replace(" ", "_")
+)
+
+
+
+df_merito_sim = df[df['Mérito'] == 'Sim'].copy()
+
+plt.figure(figsize=(10, 6))
+sns.countplot(y=df_merito_sim['Resultado'], order=df_merito_sim['Resultado'].value_counts().index, palette=['#942234', '#aa8424', '#0c326f'])
+plt.title('GRÁFICO 1 - Contagem de Processos por Resultado (Mérito = Sim)')
+plt.xlabel('Quantidade de Processos')
+plt.ylabel('Resultado da Sentença')
+st.pyplot(plt)
+plt.show()
+
