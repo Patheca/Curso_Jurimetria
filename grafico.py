6f0788e63e154a627b9d0ba1b07a08352cafd29f
Patheca <pathe.castilho@gmail.com>
2026-05-20 09:37:36 -0300

    Add initial grafico.py with data visualization setup

---
+++ grafico.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(style="whitegrid")
+
+
+url = "https://docs.google.com/spreadsheets/d/1M2t-eR0NE_vAPtjl9CBS7RINxaY3POcVoO4xQvCZnL8/export?format=xlsx"
+
+df = pd.read_excel(url, sheet_name="SENTENÇAS")
+
+# Visualizar primeiras linhas
+df.head()
+st.write('deu certo')
